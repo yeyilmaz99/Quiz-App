12c36d22eaf161d5ab10d9b04e24e44652a63d4a
yeyilmaz99 <yeyilmaz34@gmail.com>
2021-08-25 04:09:04 +0300

    first commit

---
+++ quizDemo.js
@@ -0,0 +1,108 @@
+
+
+function Question(text,choices,answer){
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+
+// question prototype
+
+Question.prototype.checkAnswer = function(answer){
+    return this.answer === answer;
+    
+}
+
+// quiz constructor
+
+function Quiz(questions){
+    this.questions = questions;
+    this.score = 0;
+    this.questionIndex = 0
+}
+
+// Quiz prototype
+
+Quiz.prototype.getQuestion = function(){
+    return this.questions[this.questionIndex];
+}
+
+// quiz isFinish
+
+Quiz.prototype.isFinish = function(){
+    return this.questions.length === this.questionIndex;
+}
+
+// quiz guess
+
+Quiz.prototype.guess = function(answer){
+    var question = this.getQuestion();
+
+    if(question.checkAnswer(answer)){
+        this.score++;
+    }
+    this.questionIndex++;
+}
+
+
+var q1 = new Question("what is the best programming language", 
+["C#", "javascript", "pyhton", "asp.net"], "javascript");
+
+var q2 = new Question("what is the most popular language?",
+["C#", "visual basic", "nodejs", "javascript"], "javascript");
+
+var q3 = new Question("what's the best modern programming language?",
+["C#", "javascript", "pyhton", "asp.net"], "javascript");
+
+
+var questions = [q1,q2,q3];
+
+
+// Start Quiz
+
+var quiz = new Quiz(questions);
+
+loadQuestion();
+
+function loadQuestion(){
+    if(quiz.isFinish()){
+        showScore();
+    }else{
+        var question = quiz.getQuestion();
+        var choices = question.choices;
+        document.querySelector('#question').textContent = question.text;
+
+        for(var i=0; i<choices.length; i++){
+            var element = document.querySelector('#choice'+i);
+            element.innerHTML = choices[i];
+
+            guess('btn'+i,choices[i]);
+
+        }
+
+        showProgress();
+    }
+}
+
+function guess(id,guess){
+    var btn = document.getElementById(id);
+    btn.onclick = function(){
+        quiz.guess(guess);
+        loadQuestion()
+    }
+};
+
+
+function showScore(){
+    var html = `<h2>Score</h2> <h4>${quiz.score}</h4>`;
+
+    document.querySelector('.card-body').innerHTML = html;
+}
+
+function showProgress(){
+    var totalQuestions= quiz.questions.length;
+    var questionNumber = quiz.questionIndex+1;
+    document.querySelector('#progress').innerHTML = 'Question' +' '+ questionNumber + ' '+ 'of'  +' '+  totalQuestions
+
+}
